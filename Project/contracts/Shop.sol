b549e0bf5f4464ea5aabfe9d6b841ebd48c27e70
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-16 15:20:24 +1100

    shop contract fix

--- Project/contracts/Shop.sol
+++ Project/contracts/Shop.sol
@@ -5,6 +5,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IERC20MintableBurnable is IERC20 {
+    function mint(address, uint256) external;
+    function burnFrom(address, uint256) external;
+
+}
+
+interface IERC721MintableBurnable is IERC721 {
+    function safeMint(address, uint256) external;
+    function burn(uint256) external;
+
+}
+
+
 contract Shop is Ownable {
     uint256 public purchaseRatio;
     uint256 public mintPrice;
