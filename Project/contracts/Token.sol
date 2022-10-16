3a7509da07198689beb61152d254ac9fb5153c5a
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-16 18:48:21 +1100

    contract fix

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -3,3 +3,5 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
