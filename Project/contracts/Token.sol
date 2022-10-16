eb9215adaae6c9250ac7d0ddb4b65803afd7ff07
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-16 18:48:45 +1100

    contract fix

--- Project/contracts/Token.sol
+++ Project/contracts/Token.sol
@@ -7,5 +7,5 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract MyToken is ERC20, Ownable, ERC20Permit, ERC20Votes {
-  
+   constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
 }
